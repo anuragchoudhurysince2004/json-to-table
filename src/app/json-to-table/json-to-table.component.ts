c24775cf5ce7c2b916f5e0727d840d7eb90fbbc6
Anurag choudhury <idanurag567@gmail.com>
2024-01-23 14:07:51 +0530

    added the search bar

--- src/app/json-to-table/json-to-table.component.ts
+++ src/app/json-to-table/json-to-table.component.ts
@@ -1,21 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-json-to-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './json-to-table.component.html',
   styleUrl: './json-to-table.component.css',
 })
 export class JsonToTableComponent {
   @Input() data: any[] = [];
+  searchText = '';
 
   get columns(): string[] {
     // Assuming all rows have the same columns
     return Object.keys(this.data.length > 0 ? this.data[0] : {});
   }
 
-  get rows(): any[] {
-    return this.data;
+  get filteredRows(): any[] {
+    return this.data.filter((row) =>
+      row.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
 }
